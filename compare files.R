9faf99b15648a540ab9510b1c42d6b0d8b050f08
Gerko Vink <g.vink@uu.nl>
2019-09-30 11:09:19 +0200

    update with aim for comparison

--- compare files.R
+++ compare files.R
@@ -2,4 +2,4 @@ modified_file <- system.file("ThesisProposal_GV.Rmd", package = "reviewer")
 reference_file <- system.file("ThesisProposal.Rmd", package = "reviewer")
 
 library(reviewer)
-result <- diff_rmd(modified_file, reference_file)
+result <- diff_rmd(current_file = "ThesisProposal_GV.Rmd", reference_file)
