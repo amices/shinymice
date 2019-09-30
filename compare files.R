5ff3b8bf1eb6b6d13a7d9e8eccec36adaaea4690
Gerko Vink <g.vink@uu.nl>
2019-09-30 11:18:09 +0200

    changed code

--- compare files.R
+++ compare files.R
@@ -1,5 +1,7 @@
-modified_file <- system.file("ThesisProposal_GV.Rmd", package = "reviewer")
-reference_file <- system.file("ThesisProposal.Rmd", package = "reviewer")
-
 library(reviewer)
-result <- diff_rmd(current_file = "ThesisProposal_GV.Rmd", reference_file)
+result <- diff_rmd(current_file = "ThesisProposal_GV.Rmd", 
+                   reference_file = "ThesisProposal.Rmd", 
+                   show = "raw")
+result2 <- diff_rmd(current_file = "ThesisProposal_GV.Rmd", 
+                   reference_file = "ThesisProposal.Rmd", 
+                   show = "rendered")
