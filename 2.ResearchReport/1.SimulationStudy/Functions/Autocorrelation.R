304c22a0fb7d6a6da8b8d8562439a802421a4ce6
Hanne <h.i.oberman@students.uu.nl>
2020-01-13 16:28:00 +0100

    clean-up research report folder and edit readme

---
+++ 2.ResearchReport/1.SimulationStudy/Functions/Autocorrelation.R
@@ -0,0 +1,37 @@
+# Compute the maximum auto-correlation at lag 1
+# Used within imputation function
+
+autocorr_function <- function(imp, maxit, m = 5, n.var = 4, moment = "mean"){
+  
+  # object for for loop
+  ac_per_chain <- numeric(length = m)
+  ac_per_var <- numeric(length = n.var)
+  names(ac_per_var) <- attr(imp$chainMean, "dimnames")[[1]]
+  
+  # make suitable for convergence of mean and variance
+  if (moment == "mean") {
+    sims <- imp$chainMean
+  } else if (moment == "variance") {
+    sims <- imp$chainVar
+  }
+  
+  # compute convergence diagnostic per variable
+  for (v in 1:n.var) {
+    for (chain in 1:m) {
+      # ac[chain, v] <-
+      ac_per_chain[chain] <-
+        acf(sims[v, , chain],
+          lag.max = 1,
+          plot = F)$acf[-1] 
+    }
+    ac_per_var[v] <- ac_per_chain[which.max(abs(ac_per_chain))]
+}
+# output
+  return(ac_per_var)
+}
+
+# NB. This function can be adjusted to show the nr. of chains with significant auto-correlations,
+# output will then be the fraction of imputation chains with significant autocorrelation at lag 1.
+# To get this, add:
+# # critical value significance 
+# AC_lim <- qnorm((1 + .95) / 2) / sqrt(maxit)
---
+++ 2.ResearchReport/README.md
@@ -0,0 +1,13 @@
+This folder contains the research archive of the research report handed in on December 15, 2019. It contains the following folders:
+
+- `1.SimulationStudy` 
+
+- `2.Presentation`
+
+- `3.QuestionMICEmeeting` 
+
+
+
+Note: *The ShinyMICE project will **not** be complete before June 2020. There will be errors. You have been warned.*
+
+-Hanne
