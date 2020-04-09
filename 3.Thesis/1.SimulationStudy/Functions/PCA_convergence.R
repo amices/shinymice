dc4466118aac422c8bdd7ae91b220fc80cd32e84
Hanne <h.i.oberman@students.uu.nl>
2020-04-09 14:12:36 +0200

    Both r hat and AC for all sim conditions, still only for 1 rep

--- 3.Thesis/1.SimulationStudy/Functions/PCA_convergence.R
+++ 3.Thesis/1.SimulationStudy/Functions/PCA_convergence.R
@@ -5,18 +5,26 @@
 PCA_convergence <- function(sims, maxit = n.iter, reps = n.sim, m = 5) { 
   PCAs <-
     map(sims, as.data.table) %>% rbindlist(fill = TRUE) %>% .[, grep("pca", names(.)), with = FALSE] %>% t() %>% as.data.frame()
-  
   sim1 <- map(PCAs, ~{
     matrix(., nrow = m, ncol = maxit) %>% t() %>% as.data.frame()
     }) 
   
-  ac <- matrix(NA, maxit, 1)
+  # loop over nr of simulations
+  
+  # loop over nr of iterations
+  rh <- ac <- matrix(NA, maxit, 1)
   for (it in 2:maxit){
+  # auto-correlation
   ac[it] <-
    sim1[[1]][1:it,] %>% acf(lag.max = 1, plot = FALSE) %>% .$acf %>% .[2, , ] %>% diag() %>% mean()
+  # r hat
+  rhat_bulk <-
+    sim1[[1]][1:it,] %>% as.matrix() %>% split_chains() %>% z_scale() %>% get.rhat(maxit = it)
+  rhat_tail <-
+    sim1[[1]][1:it,] %>% as.matrix() %>% fold_sims() %>% split_chains() %>% z_scale() %>% get.rhat(maxit = it)
+  rh[it] <- max(rhat_bulk, rhat_tail)
   }
-  
-  return(ac)
+  return(list(ac = ac, rh = rh))
 }
 
 PCA_convergence(out)
