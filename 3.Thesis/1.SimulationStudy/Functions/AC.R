b5ed6388d19b0d2e51cfd0f45294f7b014f7fbc5
Hanne <h.i.oberman@students.uu.nl>
2020-04-23 15:03:50 +0200

    Adjust function names and comments

--- 3.Thesis/1.SimulationStudy/Functions/AC.R
+++ 3.Thesis/1.SimulationStudy/Functions/AC.R
@@ -1,5 +1,5 @@
 # Extract convergence diagnostic autocorrelation from iteration object (e.g., chain means)
-# Requires package 'magrittr'/'dplyr', and the functions in Convergence_supplement.R
+# Requires packages 'purrr' and 'magrittr'/'dplyr', and the functions in AC_supplement.R
 
 # NB. This function can be adjusted to show the nr. of chains with significant auto-correlations,
 # output will then be the fraction of imputation chains with significant autocorrelation at lag 1.
@@ -44,43 +44,6 @@ ac_adapted <- function(x, ac_function = "ac") {
   }
   
   # output
-ac
+return(ac)
 }
 
-# function for lag 1 AC for one or more imputation chains
-ac_lag1 <- function(x) {
-  # input: object with theta values (rows are iterations, columns are imputations)
-  # output: one autocorrelation value per imputation (over the complete set of iterations in x)
-  
-  # make input ready for mapping
-  if (is.numeric(x)) {
-    x <- as.data.frame(x)
-  }
-  
-  # compute ac per imputation (column)
-  out <- map_dbl(1:dim(x)[2], function(m) {
-    cor(x[-dim(x)[1], m], x[-1, m])
-  }) %>% set_names(paste0("ac.", names(x)))
-  
-  # output
-  return(out)
-}
-
-# adjusted stats::acf() function for lag 1 AC for one or more imputation chains
-acf_lag1 <- function(x) {
-  # input: object with theta values (rows are iterations, columns are imputations)
-  # output: autocorrelation per imputation, with max and mean across imputations
-  
-  # make input ready for mapping
-  if (is.numeric(x)) {
-    x <- as.data.frame(x)
-  }
-  
-  # compute ac
-  out <-
-    acf(x, lag.max = 1, plot = FALSE) %>% .$acf %>% .[2, , ] %>% as.matrix() %>% diag() %>% set_names(paste0("acf.", names(x))) 
-  
-  
-  # output
-  return(out)
-}
--- 3.Thesis/1.SimulationStudy/Functions/Rhat_supplement.R
+++ 3.Thesis/1.SimulationStudy/Functions/Rhat_supplement.R
@@ -1,32 +1,64 @@
-# Extract convergence diagnostic Rhat from iteration object (e.g., chain means)
-# Requires package 'magrittr'/'dplyr', and the functions in Convergence_supplement.R
+# Supplementary functions for Rhat function (see Rhat.R)
+# To get folded, split-half, rank-normalized Rhat
 
-# function for Rhat for two or more imputation chains
-rhat_adapted <- function(x) {
-  # input: object with theta values (rows are iterations, columns are imputations)
-  # output: convergence diagnostic Rhat across imputations
+# fold chains for rhat of tails
+fold_sims <- function(sims) {
+  # fold Markov chain for rhat of tails, adapted from rstan
+  abs(sims - median(sims))
+}
+
+# split chains with maxit > 4 to detect trending
+split_chains <- function(sims) {
+  # split Markov chains, adapted from rstan
+
+  # number of iterations
+  t <- dim(sims)[1]
   
-  # make input ready for mapping
-  if (is.data.frame(x)) {
-    x <- as.matrix(x)
-  }
+  # do not split if result will be chains of length 1
+  if (t < 4)
+    return(sims) else {
+  # split each chain to get 2m chains
+  lower <- 1:floor(t/2)
+  upper <- ceiling((t/2) + 1):t
+  splits <- base::cbind(sims[lower, ], sims[upper, ])
+  return(splits)}
+}
+
+# rank-normalize chains because Gelman says so
+z_scale <- function(x) {
   
-  # quit function if there are not enough iterations
-  t <- dim(x)[1]
-  if (is.null(t)) {
-    r.hat <- data.frame(iteration = 1, max.r.hat = NA, r.hat = NA)
-  }
-  else {
-    r.hat <- map_dfr(2:t, function(it) {
-      # compute r hat in all ways described by Vehtari et al. (2019)
-      rhat_original <- x[1:it, ] %>% get.rhat()
-      rhat_bulk <-
-        x[1:it, ] %>% split_chains() %>% z_scale() %>% get.rhat()
-      rhat_tail <-
-        x[1:it, ] %>% fold_sims() %>% split_chains() %>% z_scale() %>% get.rhat()
-      max(rhat_bulk, rhat_tail) %>% data.frame(max.r.hat = ., r.hat = rhat_original) #%>% set_names("r.hat")
-    }) %>% rbind(NA, .) %>% cbind(iteration = 1:t, .)
-  }
+  # rank-normalize Markov chain, copied from rstan
+  t <- length(x)
+  r <- rank(x, ties.method = 'average')
+  z <- qnorm((r - 1 / 2) / t)
+  
   # output
-  return(r.hat)
+  if (!is.null(dim(x))) {
+    # output should have the input dimensions
+    z <- array(z, dim = dim(x), dimnames = dimnames(x))
+  }
+  return(z)
+}
+
+# compute rhat
+get.rhat <- function(sims) {
+  # compute potential scale reduction factor (rhat) for each variable in mids object
+  # equations adapted from Vehtari et al. (2019)
+  
+  # number of iterations
+  t <- length(sims)
+  
+  # between chain variance
+  var_between <-
+    t * var(apply(sims, 2, mean))
+ 
+  # within chain variance
+  var_within <- mean(apply(sims, 2, var))
+  
+  # rhat
+  rhat <-
+    sqrt((var_between / var_within + t - 1) / t)
+  
+  # output
+  return(rhat)
 }
