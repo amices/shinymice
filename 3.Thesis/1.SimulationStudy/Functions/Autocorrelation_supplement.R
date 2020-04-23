ab9ad0873d92831b00c5f9543f2a348120d5ed34
Hanne <h.i.oberman@students.uu.nl>
2020-04-23 13:26:03 +0200

    Both Rhat and AC work properly now for all iterations. Last version with old functions. Cleaning up Functions folder after this.

--- 3.Thesis/1.SimulationStudy/Functions/Autocorrelation_supplement.R
+++ 3.Thesis/1.SimulationStudy/Functions/Autocorrelation_supplement.R
@@ -2,27 +2,61 @@
 # requires library(purrr) and dplyr/magrittr
 
 # function for lag 1 AC for one or more imputation chains
-ac_lag1 <- function(x) {
-  # input: object with theta values (rows are iterations, columns are imputations)
-  # output: autocorrelation per imputation, with max and mean across imputations
+ac_adapted <- function(x, ac_function = c("ac", "acf")) {
+  # input: object with theta values (rows are iterations, columns are imputations), 
+  # and preferred way of calculating ac values 
+  # output: autocorrelations at each iteration in x, computed per imputation chain
+  # and with max and mean across imputations
+  
+  # set function to apply
+  if (ac_function = "acf"){
+    function_to_apply <- acf_lag1
+  } else {
+    function_to_apply <- ac_lag1
+  }
   
   # make input ready for mapping
   if (is.numeric(x)) {
     x <- as.data.frame(x)
   }
   
-  # compute ac
+  # quit function if there are not enough iterations
+  t <- dim(x)[1]
+  M <- dim(x)[2]
+  
+  # compute ac for each iteration (by definition only possible for t > 2)
+  if (t < 3) {
+    ac <-
+      matrix(NA, t, M + 2) %>% as.data.frame %>% set_names(paste0(ac_function, ".", c("mean", "max", names(x))))
+  } else {
+    ac <- map_dfr(3:t, function(it) {
+      # compute ac
+      x[1:it,] %>% function_to_apply() %>% t() %>% data.frame(mean(.), max(.), .) %>% set_names(c(paste0(ac_function, ".mean"), paste0(ac_function, ".max"), names(.)[-(1:2)]))
+      
+    }) %>% rbind(NA, NA, .) %>% cbind(iteration = 1:t, .)
+  }
+  
+  # output
+  return(ac)
+}
+
+# function for lag 1 AC for one or more imputation chains
+ac_lag1 <- function(x) {
+  # input: object with theta values (rows are iterations, columns are imputations)
+  # output: one autocorrelation value per imputation (over the complete set of iterations in x)
+  
+  # make input ready for mapping
+  if (is.numeric(x)) {
+    x <- as.data.frame(x)
+  }
+  
+  # compute ac per imputation (column)
   ac <- map_dbl(1:dim(x)[2], function(m) {
     cor(x[-dim(x)[1], m], x[-1, m])
   }) %>% set_names(paste0("ac.", names(x)))
   
-  # add mean and max for iterations > 2
-  if (!is.na(ac)[1]) {
-    ac <- c(ac, mean.ac = mean(ac), max.ac = max(ac))
-  }
-  
   # output
-  return(data.frame(t(ac)))
+  return(ac)
 }
 
 # adjusted stats::acf() function for lag 1 AC for one or more imputation chains
@@ -37,29 +71,10 @@ acf_lag1 <- function(x) {
   
   # compute ac
   acf <-
-    acf(x, lag.max = 1, plot = FALSE) %>% .$acf %>% .[2, , ] %>% diag() %>% set_names(paste0("acf.", names(x)))
+    acf(x, lag.max = 1, plot = FALSE) %>% .$acf %>% .[2, ,] %>% diag() %>% set_names(paste0("acf.", names(x)))
   
-  # add mean and max for iterations > 2
-  if (!is.na(acf)[1]) {
-    acf <- c(acf, mean.acf = mean(acf), max.acf = max(acf))
-  }
   
   # output
-  return(data.frame(t(acf)))
+  return(acf)
 }
 
-################
-# chain <- 1:4
-# Tt <- length(chain)
-# theta <- chain - mean(chain)
-#
-# Tt/(Tt-1) * (theta[-1] %*% theta[-Tt])/(theta[-1]%*%theta[-1])
-
-# for (t in 1:(Tt-1)){
-#   a[t] <- theta[t]*theta[t+1]
-# }
-# sum(a)/Tt
-
-#
-#
-# AC <- (theta[-length(theta)] %*% theta[-1]) / (theta[-length(theta)] %*% theta[-length(theta)])
