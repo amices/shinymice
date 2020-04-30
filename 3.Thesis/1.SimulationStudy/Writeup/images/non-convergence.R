33f49d3b0cc7147987805a689e980ceb6678cede
Hanne <h.i.oberman@students.uu.nl>
2020-04-30 22:59:06 +0200

    add regular rhat in figure 3, adjust methods section to include hypotheses and better structure with qs and thetas.

--- 3.Thesis/1.SimulationStudy/Writeup/images/non-convergence.R
+++ 3.Thesis/1.SimulationStudy/Writeup/images/non-convergence.R
@@ -37,7 +37,7 @@ save(nonc, file = "3.Thesis/1.SimulationStudy/Writeup/images/nonc_plot.Rdata")
 
 # diagnostics on chain means of wgt (rows are iterations, columns are imputations)
 diagnostics1 <-
-  imp.bmi1$chainMean[3, ,] %>% cbind(patho = "Pathological", convergence(., include_acf = TRUE), .)
+  imp.bmi1$chainMean[3, ,] %>% cbind(patho = "Non-", convergence(., include_acf = TRUE), .)
 
 #####################
 # Regular convergence
@@ -97,109 +97,130 @@ theme_update(
   legend.margin = margin(0,0,0,0),
   # adjust text size
   text = element_text(size = 8),
-  title = element_text(size = 8)
+  title = element_text(size = 8),
   # axis.title = element_text(size = 8),
   # axis.text = element_text(size = 7),
-  # legend.text = element_text(size = 7)
+  legend.title = element_text(size = 7)
 )
 
 # plot chain means
 theta <- ggplot(diagnostics) +
   geom_point(aes(x = iteration, y = `Chain 1`, color = patho),
-             size = .75,
+             size = .5,
              na.rm = TRUE) +
   geom_point(aes(x = iteration, y = `Chain 2`, color = patho),
-             size = .75,
+             size = .5,
              na.rm = TRUE) +
   geom_point(aes(x = iteration, y = `Chain 3`, color = patho),
-             size = .75,
+             size = .5,
              na.rm = TRUE) +
   geom_point(aes(x = iteration, y = `Chain 4`, color = patho),
-             size = .75,
+             size = .5,
              na.rm = TRUE) +
   geom_point(aes(x = iteration, y = `Chain 5`, color = patho),
-             size = .75,
+             size = .5,
              na.rm = TRUE) +
-  geom_line(aes(x = iteration, y = `Chain 1`, color = patho), na.rm = TRUE) +
-  geom_line(aes(x = iteration, y = `Chain 2`, color = patho), na.rm = TRUE) +
-  geom_line(aes(x = iteration, y = `Chain 3`, color = patho), na.rm = TRUE) +
-  geom_line(aes(x = iteration, y = `Chain 4`, color = patho), na.rm = TRUE) +
-  geom_line(aes(x = iteration, y = `Chain 5`, color = patho), na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = `Chain 1`, color = patho), lwd = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = `Chain 2`, color = patho), lwd = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = `Chain 3`, color = patho), lwd = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = `Chain 4`, color = patho), lwd = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = `Chain 5`, color = patho), lwd = .5, na.rm = TRUE) +
   scale_x_continuous(breaks = 1:10) +
   xlab("") +
   ylab(bquote(theta)) +
-  # ggtitle(expression(paste("Chain means (", theta, ")"))) +
-  #ggtitle(expression(paste("Scalar summary (", theta, ")"))) +
-  theme(legend.position = "")
+  labs(color = "Convergence") + 
+  theme(legend.position = "top")
 
-# plot rhat
-rhat <-
-  ggplot(diagnostics, aes(x = iteration, y = r.hat.max, color = patho)) +
-  geom_hline(yintercept = 1,
-             color = "grey",
-             lwd = 2) +
-  geom_point(size = .75, na.rm = TRUE) +
-  geom_line(na.rm = TRUE) +
-  scale_x_continuous(breaks = 1:10) +
-  xlab("") +
-  ylab(bquote(widehat(R)~" of "~theta)) +
-  #ggtitle(expression(paste(widehat(R), " of ", theta))) + 
-  labs(color = "Convergence")
-
-# plot default ac (with acf())
-acf <-
-  ggplot(diagnostics, aes(x = iteration, y = acf.max, color = patho)) +
-  geom_hline(yintercept = 0,
-             color = "grey",
-             lwd = 2) +
-  geom_point(size = .75, na.rm = TRUE) +
-  geom_line(na.rm = TRUE) +
-  scale_x_continuous(breaks = 1:10) +
-  scale_y_continuous(limits = c(-0.5,1)) +
-  xlab("Iteration") +
-  ylab(bquote("AC of "~theta~"(default)")) +
-  #ggtitle(expression(paste("AC of ", theta, " (default calculation)"))) + 
-  theme(legend.position = "")
 
 # plot manual ac
-ac <-
-  ggplot(diagnostics, aes(x = iteration, y = ac.max, color = patho)) +
-  geom_hline(yintercept = 0,
-             color = "grey",
-             lwd = 2) +
-  geom_point(size = .75, na.rm = TRUE) +
-  geom_line(na.rm = TRUE) +
+ac_both <-
+  ggplot(diagnostics) +
+  # geom_hline(yintercept = 0,
+  #            color = "grey",
+  #            lwd = 2) +
+  geom_point(aes(x = iteration, y = acf.max, color = patho), size = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = acf.max, color = patho, linetype = "Default"), lwd = .5, na.rm = TRUE) +
+  geom_point(aes(x = iteration, y = ac.max, color = patho), size = .5, na.rm = TRUE) +
+  geom_line(aes(x = iteration, y = ac.max, color = patho, linetype = "Manual"), lwd = .5, na.rm = TRUE) +
+  scale_linetype_manual("",values=c("Default"=2,"Manual"=1))+
   scale_x_continuous(breaks = 1:10) +
   scale_y_continuous(limits = c(-0.5,1)) +
+  xlab("") +
+  ylab(bquote("AC of "~theta)) +
+  # theme(legend.position = c(0.9, 0.1)) + 
+  theme(legend.position = c(0.75, 0.25))+
+  guides(color = FALSE, linetype = guide_legend(order = 1)) 
+
+
+
+# plot rhat
+old_rhat <-
+  ggplot(diagnostics, aes(x = iteration, y = r.hat, color = patho)) +
+  # geom_hline(yintercept = 1,
+  #            color = "grey",
+  #            lwd = 2) +
+  geom_point(size = .5, na.rm = TRUE) +
+  geom_line(lwd = .5, na.rm = TRUE) +
+  scale_x_continuous(breaks = 1:10) +
+  scale_y_continuous(limits = c(0.98,1.86)) +
   xlab("Iteration") +
-  ylab(bquote("AC of "~theta~"(manual)")) +
-  #ggtitle(expression(paste("AC of ", theta, " (manual calculation)"))) + 
+  ylab(bquote(widehat(R)~" of "~theta)) +
   theme(legend.position = "")
 
+
+# plot rhat
+new_rhat <-
+  ggplot(diagnostics, aes(x = iteration, y = r.hat.max, color = patho)) +
+  # geom_hline(yintercept = 1,
+  #            color = "grey",
+  #            lwd = 2) +
+  geom_point(size = .5, na.rm = TRUE) +
+  geom_line(lwd = .5, na.rm = TRUE) +
+  scale_x_continuous(breaks = 1:10) +
+  scale_y_continuous(limits = c(0.98,1.86)) +
+  xlab("Iteration") +
+  ylab(bquote(widehat(R)~" of "~theta)) +
+  theme(legend.position = "")
+
+
 # combine
-diagnostics_plot <- theta + rhat + acf  + ac + plot_layout(guides = "collect", ncol = 2) + plot_annotation(tag_levels = "A", tag_suffix = ".", title = "Performance of convergence diagnostics", subtitle = bquote(theta ~ "= chain mean in "~ y[imp][",m"])) # add 'ncol=1' for plots under each other
+diagnostics_plot <- theta + (ac_both + plot_layout(guides = "keep")) + old_rhat + new_rhat + plot_layout(guides = "collect", ncol = 2) + plot_annotation(tag_levels = "A", tag_suffix = ".") # add 'ncol=1' for plots under each other
 
 # save
 save(diagnostics_plot, file = "3.Thesis/1.SimulationStudy/Writeup/images/diagnostics_plot.Rdata")
 
-
-# # plot rhat
-# old_rhat <-
-#   ggplot(diagnostics, aes(x = iteration, y = r.hat, color = patho)) +
-#   geom_hline(yintercept = 1,
+# # plot default ac (with acf())
+# acf <-
+#   ggplot(diagnostics, aes(x = iteration, y = acf.max, color = patho)) +
+#   geom_hline(yintercept = 0,
 #              color = "grey",
 #              lwd = 2) +
 #   geom_point(size = .75, na.rm = TRUE) +
 #   geom_line(na.rm = TRUE) +
 #   scale_x_continuous(breaks = 1:10) +
-#   scale_y_continuous(limits = c(0.98,1.86)) +
-#   xlab("") +
-#   ylab(bquote(widehat(R)~" of "~theta)) +
-#   #ggtitle(expression(paste(widehat(R), " of ", theta))) + 
-#   labs(color = "Convergence")
+#   scale_y_continuous(limits = c(-0.5,1)) +
+#   xlab("Iteration") +
+#   ylab(bquote("AC of "~theta~"(default)")) +
+#   #ggtitle(expression(paste("AC of ", theta, " (default calculation)"))) + 
+#   theme(legend.position = "")
+# 
+# # plot manual ac
+# ac <-
+#   ggplot(diagnostics, aes(x = iteration, y = ac.max, color = patho)) +
+#   geom_hline(yintercept = 0,
+#              color = "grey",
+#              lwd = 2) +
+#   geom_point(size = .75, na.rm = TRUE) +
+#   geom_line(na.rm = TRUE) +
+#   scale_x_continuous(breaks = 1:10) +
+#   scale_y_continuous(limits = c(-0.5,1)) +
+#   xlab("Iteration") +
+#   ylab(bquote("AC of "~theta~"(manual)")) +
+#   #ggtitle(expression(paste("AC of ", theta, " (manual calculation)"))) + 
+#   theme(legend.position = "")
 # 
 # # plot rhat
-# new_rhat <-
+# rhat <-
 #   ggplot(diagnostics, aes(x = iteration, y = r.hat.max, color = patho)) +
 #   geom_hline(yintercept = 1,
 #              color = "grey",
@@ -207,8 +228,8 @@ save(diagnostics_plot, file = "3.Thesis/1.SimulationStudy/Writeup/images/diagnos
 #   geom_point(size = .75, na.rm = TRUE) +
 #   geom_line(na.rm = TRUE) +
 #   scale_x_continuous(breaks = 1:10) +
-#   scale_y_continuous(limits = c(0.98,1.86)) +
 #   xlab("") +
 #   ylab(bquote(widehat(R)~" of "~theta)) +
 #   #ggtitle(expression(paste(widehat(R), " of ", theta))) + 
 #   labs(color = "Convergence")
+# 
