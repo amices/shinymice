efa95be4690e82947cd4eecca3e15cbda7d2ccb7
Hanne <h.i.oberman@students.uu.nl>
2020-05-01 19:02:12 +0200

    New results section and removed bullshit from the discussion

--- 3.Thesis/1.SimulationStudy/2.CreateFigures.R
+++ 3.Thesis/1.SimulationStudy/2.CreateFigures.R
@@ -27,9 +27,10 @@ library(patchwork)
 #### UNIVARIATE ####
 ####################
 
-results <- results %>% mutate(crit = qnorm((1 + .95) / 2) / sqrt(t))
+results <- results %>% mutate(crit = qnorm((1 + .95) / 2) / sqrt(t), thresh1.01 = 1.01, thresh1.1 = 1.1, thresh1.2 = 1.2)
 results$crit[results$crit>1.2] <- NA
 results$crit[results$crit>1] <- 1
+
 
 # thresh1.1 <- data.frame(x1 = 2, x2 = 100, y1 = 1.1, y2 = 1.1)
 # + geom_segment(aes(x=x1, y=y1, xend=x2, yend=y2), data = thresh1.1)
@@ -39,7 +40,7 @@ results$crit[results$crit>1] <- 1
 mean_bias <- results %>% ggplot(aes(x = t, y = bias.mean.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   # scale_x_continuous(limits = c(0,100)) + 
@@ -53,12 +54,12 @@ mean_bias <- results %>% ggplot(aes(x = t, y = bias.mean.Y, color = as.factor(p*
 mean_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.chain.mean.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 1,
              color = "grey",
-             lwd = 2) +
-  geom_hline(yintercept = 1.01, color = "grey", linetype = "dashed", na.rm = TRUE) + 
-  # geom_hline(yintercept = 1.1, color = "grey", linetype = "dashed", na.rm = TRUE) + 
-  geom_hline(yintercept = 1.2, color = "grey", linetype = "dashed", na.rm = TRUE) + 
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.2), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.1), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.01), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
   xlab("") +
   ylab(bquote(widehat(R)~" of "~theta)) +
   #ggtitle(bquote("B) " ~ widehat(R) ~ "of chain means" ~ y[imp][",Y"])) +
@@ -67,8 +68,8 @@ mean_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.chain.mean.Y, color = as.
 mean_AC <- results %>% ggplot(aes(x = t, y = ac.max.chain.mean.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
-  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", na.rm = TRUE) + 
+             lwd = 1) +
+  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) + 
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("Number of iterations") +
@@ -81,7 +82,7 @@ mean_AC <- results %>% ggplot(aes(x = t, y = ac.max.chain.mean.Y, color = as.fac
 # mean_ACF <- results %>% ggplot(aes(x = t, y = ACF.mean.X1, color = as.factor(p*100))) +
 #   geom_hline(yintercept = 0,
 #              color = "grey",
-#              lwd = 2) +
+#              lwd = 1) +
 #   geom_point(size = .25, na.rm = TRUE) +
 #   geom_line(linetype = "dotted", na.rm = TRUE) +
 #   geom_point(aes(x = t, y = AC.mean.X1, color = as.factor(p*100)), size = .25, na.rm = TRUE) +
@@ -98,7 +99,7 @@ mean_AC <- results %>% ggplot(aes(x = t, y = ac.max.chain.mean.Y, color = as.fac
 sd_bias <- results %>% ggplot(aes(x = t, y = bias.sd.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   # geom_errorbar(
   #       aes(x = t, ymin = bias.sd.Y.LL, ymax = bias.sd.Y.UL),
   #       width = .2,
@@ -116,9 +117,12 @@ sd_bias <- results %>% ggplot(aes(x = t, y = bias.sd.Y, color = as.factor(p*100)
 var_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.chain.var.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 1,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.2), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.1), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.01), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
   xlab("") +
   ylab(bquote(widehat(R)~" of "~theta)) +
   # ggtitle(bquote(widehat(R) ~ "of chain variance Y")) + # ~ "variance in" ~ y[imp][Y])) + #sigma[y[imp]]^2
@@ -128,8 +132,8 @@ var_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.chain.var.Y, color = as.fa
 var_AC <- results %>% ggplot(aes(x = t, y = ac.max.chain.var.Y, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
-  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", na.rm = TRUE) + 
+             lwd = 1) +
+  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) + 
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("Number of iterations") +
@@ -147,7 +151,7 @@ sd_bias + var_Rh + var_AC + plot_layout(guides = "collect", ncol = 1)
 est_bias <- results %>% ggplot(aes(x = t, y = bias.est.X1, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("") +
@@ -158,9 +162,12 @@ est_bias <- results %>% ggplot(aes(x = t, y = bias.est.X1, color = as.factor(p*1
 est_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.beta, color = as.factor(p*100))) +
   geom_hline(yintercept = 1,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.2), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.1), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.01), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
   xlab("") +
   ylab(bquote(widehat(R)~" of "~theta)) +
   #ggtitle(bquote("B) " ~ widehat(R) ~ "of " ~ hat(beta[2]))) +
@@ -169,8 +176,8 @@ est_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.beta, color = as.factor(p*
 est_AC <- results %>% ggplot(aes(x = t, y = ac.max.beta, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
-  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", na.rm = TRUE) + 
+             lwd = 1) +
+  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) + 
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("Number of iterations") +
@@ -189,7 +196,7 @@ est_bias + est_Rh + est_AC + plot_layout(guides = "collect", ncol = 1)
 Rsq_bias <- results %>% ggplot(aes(x = t, y = bias.R.sq*100, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("") +
@@ -201,9 +208,12 @@ Rsq_bias <- results %>% ggplot(aes(x = t, y = bias.R.sq*100, color = as.factor(p
 PCA_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.pca, color = as.factor(p*100))) +
   geom_hline(yintercept = 1,
              color = "grey",
-             lwd = 2) +
+             lwd = 1) +
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.2), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.1), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
+  geom_line(aes(x=t, y=thresh1.01), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) +
   xlab("") +
   ylab(bquote(widehat(R)~" of "~theta)) +
   #ggtitle(bquote("B) " ~ widehat(R) ~ "of first eigenvalue" ~ Sigma ~ "{" ~ y[obs] ~ "," ~ y[imp] ~ "}")) +
@@ -213,8 +223,8 @@ PCA_Rh <- results %>% ggplot(aes(x = t, y = r.hat.max.pca, color = as.factor(p*1
 PCA_AC <- results %>% ggplot(aes(x = t, y = ac.max.pca, color = as.factor(p*100))) +
   geom_hline(yintercept = 0,
              color = "grey",
-             lwd = 2) +
-  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", na.rm = TRUE) + 
+             lwd = 1) +
+  geom_line(aes(x = t, y = crit), color = "grey", linetype = "dashed", size = .25, na.rm = TRUE) + 
   geom_point(size = .25, na.rm = TRUE) +
   geom_line(size = .25, na.rm = TRUE) +
   xlab("Number of iterations") +
@@ -233,7 +243,7 @@ Rsq_bias + PCA_Rh + PCA_AC + plot_layout(guides = "collect", ncol = 1)
 # results %>% ggplot(aes(x = t, y = cov.est.X1, color = as.factor(p*100))) +
 #   geom_hline(yintercept = .95,
 #              color = "grey",
-#              lwd = 2) +
+#              lwd = 1) +
 #   geom_point(size = .25) +
 #   geom_line() +
 #   xlab("Number of iterations") +
@@ -244,7 +254,7 @@ Rsq_bias + PCA_Rh + PCA_AC + plot_layout(guides = "collect", ncol = 1)
 # results %>% ggplot(aes(x = t, y = CIW.est.X1, color = as.factor(p*100))) +
 #   # geom_hline(yintercept = .95,
 #   #            color = "grey",
-#   #            lwd = 2) +
+#   #            lwd = 1) +
 #   geom_point(size = .25) +
 #   geom_line() +
 #   xlab("Number of iterations") +
