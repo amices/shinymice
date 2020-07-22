6732e6cdd81e8a89dc29037d2bcfa8e169058939
Oberman <h.i.oberman@uu.nl>
2020-07-22 14:31:32 +0200

    Fix light/dark themes

--- shinymice/ui.R
+++ shinymice/ui.R
@@ -8,98 +8,192 @@
 # Icons: https://fontawesome.com/icons?d=gallery&m=free
 
 library("shiny")
+library("shinythemes")
 library("mice")
 library("DT")
 library("data.table")
-library("naniar")
+#library("naniar")
 library("rmarkdown")
-library("shinycssloaders")
+#library("shinycssloaders")
 
 
 # Define UI for application that draws a histogram
-shinyUI(navbarPage(
-    title = "shinymice", theme = shinythemes::shinytheme("flatly"),
-    #sets basic virtual structure (layout function)
-    tabPanel("Data", icon = icon("file-upload"),
-             sidebarLayout(
-                 sidebarPanel(
-                     shinyjs::useShinyjs(),
-                     id = "sidebar",
-                     h2("Select a dataset"),
-                     fileInput("upload", label = h4("Upload a CSV file..."), 
-                               accept = c(
-                                   "text/csv",
+shinyUI(
+    # # for logo/name as browser icon, see https://stackoverflow.com/questions/51688463/shiny-page-title-and-image
+    # fluidPage(
+    # theme = shinytheme("paper"),
+    # tags$head(HTML("<title>Baller Lab</title>")), #Without company logo
+    # #tags$head(HTML("<title>Baller Lab</title> <link rel='icon' type='image/gif/png' href='ballerlablogo.png'>")), #WIth company logo
+    # #navbarPage(title = div(img(src="ballerlablogo.png", style="margin-top: -14px;", height = 50))))
+    
+    navbarPage(
+        #tags$head(tags$style(HTML(".navbar-default .navbar-brand:hover {color: blue;}"))), # or ul.nav a:hover { color: #fff !important; }
+        title = "shinymice",
+        theme = shinythemes::shinytheme("flatly"),
+        
+        collapsible = TRUE,
+        #sets basic virtual structure (layout function)
+        tabPanel(
+            "Data",
+            icon = icon("file-upload"),
+            sidebarLayout(
+                sidebarPanel(
+                    shinyjs::useShinyjs(),
+                    id = "sidebar",
+                    h2("Select a dataset"),
+                    fileInput(
+                        "upload",
+                        label = h4("Upload a CSV file..."),
+                        accept = c("text/csv",
                                    "text/comma-separated-values,text/plain",
                                    ".csv")
-                     ),
-                     checkboxInput("header", label = "CSV file contains variable names", value = TRUE),
-                     selectInput(
-                         #is the input of the app that the user interacts with
-                         "choice",
-                         label = h4("...or use `mice` data"),
-                         choices = data(package = "mice")$results[-c(5,7, 17, 18), "Item"]
-                     ),
-                     actionButton("reset", "Reset", icon = icon("redo")),
-                 ),
-                 mainPanel(
-                     h2("Tabulated dataset"),
-                     helpText("Sort variables descending to view missing values."),
-                     DT::DTOutput("table"))
-             )),
-    tabPanel(
-        "Explore", icon = icon("th"), #icon("bar-chart-o") or table
-        h2("Observed missingness pattern per variable"),
-        helpText("Observed data is blue, missing data is red."),
-        plotOutput("md_pattern", height = "150%", width = "100%"), 
-        style = 'width:100%;height:85vh;overflow-y: scroll;'
-    ),
-    tabPanel(
-        "Impute", icon = icon("calculator"), #icon("list-alt")
-        h2("Impute missing data using `mice`"),
-        tags$b("Dataset to impute"),
-        verbatimTextOutput("names"),
-        helpText("Showing only the first 5 variable names to check what data is used."),
-        numericInput("m", label = "Number of imputations", value = 5, min = 1, step = 1),
-        numericInput("maxit", label = "Number of iterations", value = 5, min = 1, step = 1),
-        actionButton("mice", "Impute", icon = icon("hourglass-start")),
-        helpText("This may take a minute."),
-        verbatimTextOutput("done")
-        #plotOutput("traceplot")
-    ),
-    tabPanel(
-        "Evaluate", icon = icon("chart-line"), #icon("list-alt")
-        h2("Evaluate convergence"),
-        plotOutput("traceplot")
-    ),
-    navbarMenu(
-        "More", icon = icon("ellipsis-h"),
-        # tabPanel(
-        #     "Summary", 
-        #     h2("Descriptive statistics per variable"),
-        #     helpText("Check the number of NAs."),
-        #     verbatimTextOutput(#where to place the output code
-        #         "summary")),
-        tabPanel("About", icon = icon("info-circle"),
-                 h2("About this app"),
-                 fluidRow(
-                     column(6,
-                            includeMarkdown("about.Rmd")),
-                     column(
-                         3,
-                         img(
-                             class = "img-polaroid",
-                             src = paste0(
-                                 "https://raw.githubusercontent.com/gerkovink/shinyMice/edits/ICML/shinymicehex.png"
+                    ),
+                    checkboxInput("header", label = "CSV file contains variable names", value = TRUE),
+                    selectInput(
+                        #is the input of the app that the user interacts with
+                        "choice",
+                        label = h4("...or use `mice` data"),
+                        choices = data(package = "mice")$results[-c(5, 7, 17, 18), "Item"]
+                    ),
+                    actionButton("reset", "Reset", icon = icon("redo")),
+                ),
+                mainPanel(
+                    checkboxInput(inputId = "themeToggle",
+                                  label = icon("moon"),
+                                  value = FALSE),
+                    h2("Tabulated dataset"),
+                    helpText("Sort variables descending to view missing values."),
+                    DT::DTOutput("table")
+                )
+            )
+        ),
+        tabPanel(
+            "Explore",
+            icon = icon("th"),
+            #icon("bar-chart-o") or table
+            h2("Observed missingness pattern per variable"),
+            helpText("Observed data is blue, missing data is red."),
+            plotOutput("md_pattern", height = "150%", width = "100%"),
+            style = 'width:100%;height:85vh;overflow-y: scroll;'
+        ),
+        tabPanel(
+            "Impute",
+            icon = icon("calculator"),
+            #icon("list-alt")
+            h2("Impute missing data using `mice`"),
+            tags$b("Dataset to impute"),
+            verbatimTextOutput("names"),
+            helpText("Showing only the first 5 variable names to check what data is used."),
+            numericInput(
+                "m",
+                label = "Number of imputations",
+                value = 5,
+                min = 1,
+                step = 1
+            ),
+            numericInput(
+                "maxit",
+                label = "Number of iterations",
+                value = 5,
+                min = 1,
+                step = 1
+            ),
+            actionButton("mice", "Impute", icon = icon("hourglass-start")),
+            helpText("This may take a minute."),
+            verbatimTextOutput("done") #%>% withSpinner(color="#0dc5c1")
+            #plotOutput("traceplot")
+        ),
+        tabPanel(
+            "Evaluate",
+            icon = icon("chart-line"),
+            #icon("list-alt")
+            h2("Evaluate convergence"),
+            plotOutput("traceplot")
+        ),
+        navbarMenu(
+            "More",
+            icon = icon("ellipsis-h"),
+            # tabPanel(
+            #     "Summary",
+            #     h2("Descriptive statistics per variable"),
+            #     helpText("Check the number of NAs."),
+            #     verbatimTextOutput(#where to place the output code
+            #         "summary")),
+            tabPanel(
+                "About",
+                icon = icon("info-circle"),
+                h2("About this app"),
+                fluidRow(column(6,
+                                includeMarkdown("www/about.Rmd")),
+                         column(
+                             3,
+                             img(
+                                 class = "img-polaroid",
+                                 src = paste0(
+                                     "https://raw.githubusercontent.com/gerkovink/shinyMice/edits/ICML/shinymicehex.png"
+                                 ),
+                                 style = paste0("width:100%;")
                              ),
-                             style = paste0("width:100%;")
-                         ),
-                         tags$small(
-                             "Impression of the hex sticker ",
-                             "for the interactive evaulation ",
-                             "suite for multiple imputation 'ShinyMICE'",
-                             a(href = "https://github.com/gerkovink/shinyMice")
-                         )
-                     )
-                 ))
+                             tags$small(
+                                 "Impression of the hex sticker ",
+                                 "for the interactive evaulation ",
+                                 "suite for multiple imputation 'ShinyMICE'",
+                                 a(href = "https://github.com/gerkovink/shinyMice")
+                             )
+                         ))
+            ),
+            tags$script( #for light/dark theme, see https://stackoverflow.com/questions/61632272/r-shiny-light-dark-mode-switch
+                "
+        // define css theme filepaths
+        const themes = {
+            dark: 'shinythemes/css/darkly.min.css',
+            light: 'shinythemes/css/flatly.min.css'
+        }
+
+        // function that creates a new link element
+        function newLink(theme) {
+            let el = document.createElement('link');
+            el.setAttribute('rel', 'stylesheet');
+            el.setAttribute('text', 'text/css');
+            el.setAttribute('href', theme);
+            return el;
+        }
+
+        // function that remove <link> of current theme by href
+        function removeLink(theme) {
+            let el = document.querySelector(`link[href='${theme}']`)
+            return el.parentNode.removeChild(el);
+        }
+
+        // define vars
+        const darkTheme = newLink(themes.dark);
+        const lightTheme = newLink(themes.light);
+        const head = document.getElementsByTagName('head')[0];
+        const toggle = document.getElementById('themeToggle');
+
+        // define extra css and add as default
+        const extraDarkThemeCSS = '.dataTables_length label, .dataTables_filter label, .dataTables_info {       color: white!important;} .paginate_button { background: white!important;} thead { color: white;}'
+        const extraDarkThemeElement = document.createElement('style');
+        extraDarkThemeElement.appendChild(document.createTextNode(extraDarkThemeCSS));
+        head.appendChild(extraDarkThemeElement);
+
+
+        // define event - checked === 'light'
+        toggle.addEventListener('input', function(event) {
+            // if checked, switch to light theme
+            if (toggle.checked) {
+                // else add darktheme
+                removeLink(themes.light);
+                head.appendChild(extraDarkThemeElement)
+                head.appendChild(darkTheme);
+            }  else {
+                removeLink(themes.dark);
+                head.removeChild(extraDarkThemeElement);
+                head.appendChild(lightTheme);
+            }
+        })
+        "
+            )
+        )
     )
-))
+)
