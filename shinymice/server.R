9c102c2d4c60399a95ef4fcfc436f8e32beb3783
Oberman <h.i.oberman@uu.nl>
2020-07-24 09:21:32 +0200

    clean-up code, comments, and alignment

--- shinymice/server.R
+++ shinymice/server.R
@@ -1,23 +1,22 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-
-
+# shinymice server
 options(htmlwidgets.TOJSON_ARGS = list(na = 'string')) #to show NA values in dt, see https://github.com/rstudio/DT/issues/496
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-    
-    rv <- reactiveValues(data = NULL, mids = NULL, m = NULL, maxit = NULL, done = NULL)
+    rv <-
+        reactiveValues(
+            data = NULL,
+            mids = NULL,
+            m = NULL,
+            maxit = NULL,
+            done = NULL
+        )
     
     observe({
         if (is.null(input$upload)) {
             rv$data <- get(input$choice, "package:mice")
-        } else {rv$data <- read.csv(input$upload$datapath, header = input$header)}
+        } else {
+            rv$data <- read.csv(input$upload$datapath, header = input$header)
+        }
     })
     
     observeEvent(input$reset, {
@@ -28,65 +27,78 @@ shinyServer(function(input, output, session) {
     observe({
         if (is.null(input$m)) {
             rv$m <- NULL
-        } else {rv$m <- input$m}
+        } else {
+            rv$m <- input$m
+        }
     })
     
     observe({
         if (is.null(input$maxit)) {
             rv$maxit <- NULL
-        } else {rv$maxit <- input$maxit}
+        } else {
+            rv$maxit <- input$maxit
+        }
     })
     
     output$table <-
-        renderDT({rv$data %>% 
-                datatable(options = list(pageLength = 5)) %>% 
-            formatStyle(
-                names(rv$data ),
-                target = "cell",
-                color = styleEqual("NA", "#B61A51"),
-                fontWeight = styleEqual("NA", "bold")
-            )}, server = F)
-    #display the actual data frame in a table
-    # dataset <- get(input$dataset, "package:datasets") #redundant
+        renderDT({
+            rv$data %>%
+                datatable(options = list(pageLength = 5)) %>%
+                formatStyle(
+                    names(rv$data),
+                    target = "cell",
+                    color = styleEqual("NA", "#B61A51"),
+                    fontWeight = styleEqual("NA", "bold")
+                )
+        }, server = F)
     
     output$md_pattern <-
         renderPlot({
             md.pattern(rv$data)
-        }#, height = function() {2*session$clientData$output_md_pattern_width}
-    )
-    
-    output$summary <-
-        renderPrint({
-            #display a statistical summary of the data with fixed-width (verbatim) text
-            # dataset <- get(input$dataset, "package:datasets") #redundant, after adding reactive and () after dataset
-            summary(rv$data)
         })
-
+    
+    # output$summary <-
+    #     renderPrint({
+    #         #display a statistical summary of the data with fixed-width (verbatim) text
+    #         summary(rv$data)
+    #     })
+    
     output$names <-
         renderPrint({
-            #display a statistical summary of the data with fixed-width (verbatim) text
-            # dataset <- get(input$dataset, "package:datasets") #redundant, after adding reactive and () after dataset
             names(rv$data)[1:5]
         })
     
     observeEvent(input$mice, {
-        #rv$mids <- list(1)
-        rv$mids <- mice(rv$data, m = input$m, maxit = rv$maxit, printFlag = FALSE)
+        rv$mids <-
+            mice(
+                rv$data,
+                m = input$m,
+                maxit = rv$maxit,
+                printFlag = FALSE
+            )
     })
     
     observe({
         if (is.null(rv$mids)) {
-            rv$done <- "not done yet..."} 
-        #else if (rv$mids == 1) {rv$done <- "processing..."}
-        else {rv$done <- "done!"}
+            rv$done <- "not done yet..."
+        }
+        else {
+            rv$done <- "done!"
+        }
     })
     
     output$done <- renderPrint(rv$done)
     
     observe({
         if (is.null(rv$mids)) {
-            rv$trace <- ggplot(data.frame(x=NA, y=NA), aes(x, y))  + labs(x = "", y = "", title = "No imputations to evaluate.") + theme_classic()} 
-        else {rv$trace <- gg.mids(rv$mids)}
+            rv$trace <-
+                ggplot(data.frame(x = NA, y = NA), aes(x, y))  + labs(x = "",
+                                                                      y = "",
+                                                                      title = "No imputations to evaluate.") + theme_classic()
+        }
+        else {
+            rv$trace <- gg.mids(rv$mids)
+        }
     })
     
     output$traceplot <- renderPlot({
--- shinymice/ui.R
+++ shinymice/ui.R
@@ -1,12 +1,12 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-# Icons: https://fontawesome.com/icons?d=gallery&m=free
+# shinymice UI
 
+# TO DO:
+# menu bar naar dropdown
+# data of csv of rdata file --> met verschillende mids objecten om te evalueren
+# nieuwe kleuren: blauw #006CC2, rood #B61A51
+# lighter blue #66a6da, lighter red #d37596
+
+# set-up
 library("shiny")
 library("shinythemes")
 library("mice")
@@ -17,20 +17,19 @@ library("rmarkdown")
 library("ggplot2")
 #library("shinycssloaders")
 
-# menu bar naar dropdown
-# data of csv of rdata file --> met verschillende mids objecten om te evalueren
-# nieuwe kleuren: blauw #006CC2, rood #B61A51
-# lighter blue #66a6da, lighter red #d37596
-
-# Define UI for application that draws a histogram
 shinyUI(
-    fluidPage(title = "shinymice", 
-        list(
-            tags$head(
-                HTML('<link rel="icon" href="logo_square.png" 
-                type="image/png" />'))),
-        tags$head(
-            tags$style(HTML("#themeToggle, .visually-hidden {
+    fluidPage(
+        title = "shinymice",
+        theme = shinythemes::shinytheme("flatly"),
+        # for logo/name as browser icon, see https://stackoverflow.com/questions/51688463/shiny-page-title-and-image
+        list(tags$head(
+            HTML('<link rel="icon" href="logo_square.png"
+                type="image/png" />')
+        )),
+        shinyjs::useShinyjs(),
+        tags$head(tags$style(
+            HTML(
+                "#themeToggle, .visually-hidden {
     position: absolute;
     width: 1px;
     height: 1px;
@@ -38,149 +37,156 @@ shinyUI(
     clip: rect(0, 0, 0, 0);
     overflow: hidden;
 };
-    "))
-        ),
-        # add logo in right corner, see https://stackoverflow.com/questions/48529495/placing-an-image-on-right-end-of-navigation-bar-in-navbarpage-layout-in-r-shin
-        # tagList(
-        #     tags$head(tags$script(type="text/javascript", src = "logo.js")),
-        navbarPage(
-        title = div(img(src="logo_wide.png", style = "width:11%;position:fixed;"), ".................."), #, style="float:right; padding-right:25px", width = "10%"), "shinymice"), 
-        selected = "Data",
-        # add theme toggle in header, for themes, see below, for placement see https://stackoverflow.com/questions/56873774/change-css-properties-of-shiny-checkbox
-        header = div(checkboxInput(
-            inputId = "themeToggle",
-            label = icon("moon"),
-            value = FALSE, width = "150%"), 
-            style = "text-align:right;" #add this? "; background-color: blue"
-            ),
-        shinyjs::useShinyjs(),
-        # # for logo/name as browser icon, see https://stackoverflow.com/questions/51688463/shiny-page-title-and-image
-        # # does not work: div("Report", img(src = "www/logo.png", height = "10px", style = "position: relative; top: -3px; left: -1000px;")),#
-        # # for right aligned navbar, see https://stackoverflow.com/questions/35584644/r-shiny-navbarpage-right-aligned-tabs
-        # tags$head(
-        #     tags$style(HTML("
-        #   .navbar .navbar-nav {float: right}
-        #   .navbar .navbar-header {float: right}
-        # "))),
-        theme = shinythemes::shinytheme("flatly"),
-        collapsible = TRUE, #make 'hamburger' menu on small screens
-        tabPanel(
-            title = "Data",
-            icon = icon("file-upload"),
-            sidebarLayout(
-                sidebarPanel(
-                    id = "sidebar",
-                    h2("Select a dataset"),
-                    fileInput(
-                        "upload",
-                        label = h4("Upload a CSV file..."),
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                    ),
-                    checkboxInput("header", label = "CSV file contains variable names", value = TRUE),
-                    selectInput(
-                        #is the input of the app that the user interacts with
-                        "choice",
-                        label = h4("...or use `mice` data"),
-                        choices = data(package = "mice")$results[-c(5, 7, 17, 18), "Item"]
-                    ),
-                    actionButton("reset", "Reset", icon = icon("redo")),
-                ),
-                mainPanel(
-                    h2("Tabulated dataset"),
-                    helpText("Sort variables descending to view missing values."),
-                    DT::DTOutput("table")
-                )
+    "
             )
-        ),
-        tabPanel(
-            "Explore",
-            icon = icon("th"),
-            #icon("bar-chart-o") or table
-            h2("Observed missingness pattern per variable"),
-            helpText("Observed data is blue, missing data is red."),
-            plotOutput("md_pattern", height = "150%", width = "100%"),
-            style = 'width:100%;height:85vh;overflow-y: scroll;'
-        ),
-        tabPanel(
-            "Impute",
-            icon = icon("calculator"),
-            #icon("list-alt")
-            h2("Impute missing data using `mice`"),
-            tags$b("Dataset to impute"),
-            verbatimTextOutput("names"),
-            helpText("Showing only the first 5 variable names to check what data is used."),
-            numericInput(
-                "m",
-                label = "Number of imputations",
-                value = 5,
-                min = 1,
-                step = 1
+        )),
+        
+        navbarPage(
+            title = div(
+                img(src = "logo_wide.png", style = "width:11%;position:fixed;"),
+                ".............................."
             ),
-            numericInput(
-                "maxit",
-                label = "Number of iterations",
-                value = 5,
-                min = 1,
-                step = 1
+            #, style="float:right; padding-right:25px", width = "10%"), "shinymice"),
+            # for right aligned navbar, see https://stackoverflow.com/questions/35584644/r-shiny-navbarpage-right-aligned-tabs
+            collapsible = TRUE,
+            #to make 'hamburger' menu on small screens
+            selected = "Data",
+            # add theme toggle in header, for themes, see below, for placement see https://stackoverflow.com/questions/56873774/change-css-properties-of-shiny-checkbox
+            header = div(
+                checkboxInput(
+                    inputId = "themeToggle",
+                    label = icon("moon"),
+                    value = FALSE,
+                    width = "150%"
+                ),
+                style = "text-align:right;"
             ),
-            actionButton("mice", "Impute", icon = icon("hourglass-start")),
-            helpText("This may take a minute."),
-            verbatimTextOutput("done") #%>% withSpinner(color="#0dc5c1")
-            #plotOutput("traceplot")
-        ),
-        tabPanel(
-            "Evaluate",
-            icon = icon("chart-line"),
-            #icon("list-alt")
-            h2("Evaluate convergence"),
-            plotOutput("traceplot")
-        ),
-        # tabPanel(checkboxInput(inputId = "themeToggle",
-        #                        label = icon("moon"),
-        #                        value = FALSE)),
-        navbarMenu(
-            "More",
-            icon = icon("ellipsis-h"),
-            # make options appear on hover, see https://stackoverflow.com/questions/34597421/dropdown-bootstrap-menu-with-unfold-effect
-            tags$script(HTML("$('.navbar .dropdown').hover(function() {
+            
+            tabPanel(
+                title = "Data",
+                icon = icon("file-upload"),
+                sidebarLayout(
+                    sidebarPanel(
+                        id = "sidebar",
+                        h2("Select a dataset"),
+                        fileInput(
+                            "upload",
+                            label = h4("Upload a CSV file..."),
+                            accept = c("text/csv",
+                                       "text/comma-separated-values,text/plain",
+                                       ".csv")
+                        ),
+                        checkboxInput("header", label = "CSV file contains variable names", value = TRUE),
+                        selectInput(
+                            #is the input of the app that the user interacts with
+                            "choice",
+                            label = h4("...or use `mice` data"),
+                            choices = data(package = "mice")$results[-c(5, 7, 17, 18), "Item"]
+                        ),
+                        actionButton("reset", "Reset", icon = icon("redo")),
+                    ),
+                    mainPanel(
+                        h2("Tabulated dataset"),
+                        helpText("Sort variables descending to view missing values."),
+                        DT::DTOutput("table")
+                    )
+                )
+            ),
+            
+            tabPanel(
+                "Explore",
+                icon = icon("th"),
+                #icon("bar-chart-o") or table
+                h2("Observed missingness pattern per variable"),
+                helpText("Observed data is blue, missing data is red."),
+                plotOutput("md_pattern", height = "150%", width = "100%"),
+                style = 'width:100%;height:85vh;overflow-y: scroll;'
+            ),
+            
+            tabPanel(
+                "Impute",
+                icon = icon("calculator"),
+                #icon("list-alt")
+                h2("Impute missing data using `mice`"),
+                tags$b("Dataset to impute"),
+                verbatimTextOutput("names"),
+                helpText("Showing only the first 5 variable names to check what data is used."),
+                numericInput(
+                    "m",
+                    label = "Number of imputations",
+                    value = 5,
+                    min = 1,
+                    step = 1
+                ),
+                numericInput(
+                    "maxit",
+                    label = "Number of iterations",
+                    value = 5,
+                    min = 1,
+                    step = 1
+                ),
+                actionButton("mice", "Impute", icon = icon("hourglass-start")),
+                helpText("This may take a minute."),
+                verbatimTextOutput("done") #%>% withSpinner(color="#0dc5c1")
+                #plotOutput("traceplot")
+            ),
+            
+            tabPanel(
+                "Evaluate",
+                icon = icon("chart-line"),
+                #icon("list-alt")
+                h2("Evaluate convergence"),
+                plotOutput("traceplot")
+            ),
+            
+            navbarMenu(
+                "More",
+                icon = icon("ellipsis-h"),
+                # make options appear on hover, see https://stackoverflow.com/questions/34597421/dropdown-bootstrap-menu-with-unfold-effect
+                tags$script(
+                    HTML(
+                        "$('.navbar .dropdown').hover(function() {
                 $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
             }, function() {
                 $(this).find('.dropdown-menu').first().stop(true, true).slideUp()
-            });")),
-            # tabPanel(
-            #     "Summary",
-            #     h2("Descriptive statistics per variable"),
-            #     helpText("Check the number of NAs."),
-            #     verbatimTextOutput(#where to place the output code
-            #         "summary")),
-            tabPanel(
-                "About",
-                icon = icon("info-circle"),
-                h2("About this app"),
-                fluidRow(column(6,
-                                includeMarkdown("www/about.Rmd")),
-                         column(
-                             3,
-                             img(
-                                 class = "img-polaroid",
-                                 src = paste0(
-                                     "https://raw.githubusercontent.com/gerkovink/shinyMice/edits/ICML/shinymicehex.png"
+            });"
+                    )
+                ),
+                
+                # tabPanel(
+                #     "Summary",
+                #     h2("Descriptive statistics per variable"),
+                #     helpText("Check the number of NAs."),
+                #     verbatimTextOutput(#where to place the output code
+                #         "summary")),
+                
+                tabPanel(
+                    "About",
+                    icon = icon("info-circle"),
+                    h2("About this app"),
+                    fluidRow(column(6,
+                                    includeMarkdown("www/about.Rmd")),
+                             column(
+                                 3,
+                                 img(
+                                     class = "img-polaroid",
+                                     src = paste0(
+                                         "https://raw.githubusercontent.com/gerkovink/shinyMice/edits/ICML/shinymicehex.png"
+                                     ),
+                                     style = paste0("width:100%;")
                                  ),
-                                 style = paste0("width:100%;")
-                             ),
-                             tags$small(
-                                 "Impression of the hex sticker ",
-                                 "for the interactive evaulation ",
-                                 "suite for multiple imputation 'ShinyMICE'",
-                                 a(href = "https://github.com/gerkovink/shinyMice")
-                             )
-                         ))
-            ),
-            tags$script(
-                #for light/dark theme, see https://stackoverflow.com/questions/61632272/r-shiny-light-dark-mode-switch
-                "
+                                 tags$small(
+                                     "Impression of the hex sticker ",
+                                     "for the interactive evaulation ",
+                                     "suite for multiple imputation 'ShinyMICE'",
+                                     a(href = "https://github.com/gerkovink/shinyMice")
+                                 )
+                             ))
+                ),
+                
+                tags$script(
+                    #for light/dark theme, see https://stackoverflow.com/questions/61632272/r-shiny-light-dark-mode-switch
+                    "
         // define css theme filepaths
         const themes = {
             dark: 'shinythemes/css/darkly.min.css',
@@ -230,7 +236,8 @@ shinyUI(
             }
         })
         "
+                )
             )
         )
     )
-))
+)
