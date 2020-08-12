62d6a0d083fbabb9afe7d4c11441b327d689c5bc
Oberman <h.i.oberman@uu.nl>
2020-08-12 14:26:23 +0200

    All axes correct md pattern plot, but with 1 square offset below (to fix still)

--- R/plot.md.pattern.R
+++ R/plot.md.pattern.R
@@ -3,10 +3,32 @@
 # library(mice)
 # library(tidyverse)
 plot_md_pattern <- function(data) {
+  # parse inputs
   md <- mice::md.pattern(data, plot = FALSE)
   vars <- colnames(md)[-ncol(md)]
   mice_cols <- c("1" = mice:::mdc(1), "0" = mice:::mdc(2))
   second_x <- sec_axis(~.+0, breaks = seq_len(nrow(md)-1), labels = rev(as.character(md[-nrow(md),ncol(md)])), name = "Number of missing values per pattern")
+  second_y <- as.character(md[nrow(md), -ncol(md)])
+  # define function to add second discrete axis, see https://github.com/tidyverse/ggplot2/issues/3171
+  # guide_axis_label_trans <- function(label_trans = identity, ...) {
+  #   axis_guide <- guide_axis(...)
+  #   axis_guide$label_trans <- rlang::as_function(label_trans)
+  #   class(axis_guide) <- c("guide_axis_trans", class(axis_guide))
+  #   axis_guide
+  # }
+  # 
+  # guide_train.guide_axis_trans <- function(x, ...) {
+  #   trained <- NextMethod()
+  #   trained$key$.label <- x$label_trans(trained$key$.label)
+  #   trained
+  # }
+  
+  # make dataset with var names
+  var_lab <- data.frame(
+    name = vars, 
+    label = vars,
+    value = 1)
+
   # process and plot
   md[1:nrow(md) - 1, 1:ncol(md) - 1] %>%
     cbind(id = 1:nrow(.)) %>%
@@ -18,12 +40,16 @@ plot_md_pattern <- function(data) {
       fill = factor(value)
     )) +
     ggplot2::geom_tile(colour = "black") +
-    ggplot2::scale_x_discrete(limits = vars, expand = c(0, 0), position = "top") +
-    ggplot2::scale_y_continuous(expand = c(0, 0), breaks = seq_len(nrow(md)-1), labels = rev(rownames(md)[-nrow(md)]), sec.axis = second_x) +
+    ggplot2::scale_x_discrete(limits = vars, expand = c(0, 0), position = "bottom", labels = second_y) +
+    ggplot2::scale_y_continuous(breaks = seq_len(nrow(md)-1), labels = rev(rownames(md)[-nrow(md)]), sec.axis = second_x) +
     ggplot2::theme_classic() +
     ggplot2::scale_fill_manual(values = mice_cols) +
-    ggplot2::theme(legend.position = "bottom") + 
-    labs(x = "Variable", y = "Number of rows with this pattern") +
+    labs(
+      x = "Total number of missing values per variable", 
+      y = "Number of rows with this pattern",
+      title = " ") +
+    geom_text(aes(x = name, y = Inf, label = label), data = var_lab, vjust = 0) +
+    #guides(x.sec = guide_axis_label_trans()) +
     theme(legend.position = "none")
     #geom_text(aes(name, y = -Inf, label = label), data = data.frame(name = vars, label = as.character(md[nrow(md), -ncol(md)])), vjust = -0.5) 
 }
