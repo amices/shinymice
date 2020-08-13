4c74f6786792feb3a2122d1c7da4b5bc726d4ad1
Hanne Oberman <h.i.oberman@uu.nl>
2020-08-13 07:46:28 +0000

    Scatterplot wit NAs on x axis

--- R/plot.NA.data.R
+++ R/plot.NA.data.R
@@ -5,7 +5,15 @@ data <- mice::boys[sample.int(748, 100),]
 
 v = "hc"
 
-data %>% mutate(R = is.na(.data[[v]]), hc = factor(.data[[v]], exclude = NULL)) %>% 
+# data %>% mutate(R = !is.na(.data[[v]]), hc = factor(.data[[v]], exclude = NULL)) %>% 
+#   ggplot() +
+#   geom_point(aes(x = rownames(data), y = .data[[v]], color = R))  +
+#   scale_x_discrete(limits = c(levels(.data[[v]])))
+
+NA_level <- min(data[[v]], na.rm = TRUE) - sd(data[[v]], na.rm = TRUE)
+data %>% mutate(R = ifelse(is.na(.data[[v]]), NA_level, NA), id = 1:nrow(data)) %>% 
   ggplot() +
-  geom_point(aes(x = rownames(data), y = .data[[v]], color = R))  +
-  scale_x_discrete(limits = c(levels(.data[[v]])))
+  geom_point(aes(x = id, y = .data[[v]]), color = mice:::mdc(1), na.rm = TRUE) +
+  geom_point(aes(x = id, y = R), color = mice:::mdc(2), na.rm = TRUE) + 
+  scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
+  theme_classic()
