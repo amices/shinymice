6e29538b9d9a67e57edf8ee7d039e2cc1cb88a1d
Hanne Oberman <h.i.oberman@uu.nl>
2020-08-13 13:58:09 +0000

    Working version NA plot for any input data (only missing points for cells that are missing in both x and y var)

--- R/plot.NA.data.R
+++ R/plot.NA.data.R
@@ -8,70 +8,103 @@ plot_NA_margins <- function(data, x, y = NULL) {
   }
   
   # preprocessing
-  geom_list <- scale_list <- list(NULL)
-  if(any(is.na(data[[x]]))){
-    geom_list <- list(geom_point(aes(x = R_x, y = .data[[y]]),
-                      color = mice:::mdc(2),
-                      na.rm = TRUE))
-    if(is.numeric(data[[x]])){
-      data <- data %>% mutate(R_x = ifelse(is.na(.data[[x]]), NA_level_x, NA))
-      scale_list <- list(scale_x_continuous(expand = expansion(mult = c(0.01, .05))))
+  geom_x_list <-
+    geom_y_list <- scale_x_list <- scale_y_list <- list(NULL)
+  if (any(is.na(data[[x]]))) {
+    geom_x_list <- list(geom_point(
+      aes(x = R_x, y = .data[[y]]),
+      color = mice:::mdc(2),
+      na.rm = TRUE
+    ))
+    if (is.numeric(data[[x]])) {
+      NA_level_x <-
+        min(data[[x]], na.rm = TRUE) - sd(data[[x]], na.rm = TRUE)
+      data <-
+        data %>% mutate(R_x = ifelse(is.na(.data[[x]]), NA_level_x, NA))
+      scale_x_list <-
+        list(scale_x_continuous(expand = expansion(mult = c(0.01, .05))))
     } else {
       data <- data %>% mutate(R_x = ifelse(is.na(.data[[x]]), "   ", NA))
-      scale_list <- list(scale_x_discrete(expand = expansion(mult = c(0.01, .05))))
+      scale_x_list <-
+        list(scale_x_discrete(expand = expansion(mult = c(0.01, .05))))
     }
   }
   
-  # do not do any further processing if there are no NAs
-  if (all(!is.na(data[[x]])) & all(!is.na(data[[y]]))) {
-    return(ggplot(data, aes(x = .data[[x]], y = .data[[y]])) +
-             geom_point(color = mice:::mdc(1)) +
-             theme_classic())
+  if (any(is.na(data[[y]]))) {
+    geom_y_list <- list(geom_point(
+      aes(x = .data[[x]], y = R_y),
+      color = mice:::mdc(2),
+      na.rm = TRUE
+    ))
+    if (is.numeric(data[[y]])) {
+      NA_level_y <-
+        min(data[[y]], na.rm = TRUE) - sd(data[[y]], na.rm = TRUE)
+      data <-
+        data %>% mutate(R_y = ifelse(is.na(.data[[y]]), NA_level_y, NA))
+      scale_y_list <-
+        list(scale_y_continuous(expand = expansion(mult = c(0.01, .05))))
+    } else {
+      data <- data %>% mutate(R_y = ifelse(is.na(.data[[y]]), "   ", NA))
+      scale_y_list <-
+        list(scale_y_discrete(expand = expansion(mult = c(0.01, .05))))
+    }
   }
   
+  
+  # do not do any further processing if there are no NAs
+  # if (all(!is.na(data[[x]])) & all(!is.na(data[[y]]))) {
+  #   return(ggplot(data, aes(x = .data[[x]], y = .data[[y]])) +
+  #            geom_point(color = mice:::mdc(1)) +
+  #            theme_classic())
+  # }
+  
   # for continuous variables
-  if (is.numeric(data[[y]])) {
-    NA_level_x <-
-      min(data[[x]], na.rm = TRUE) - sd(data[[x]], na.rm = TRUE)
-    NA_level_y <-
-      min(data[[y]], na.rm = TRUE) - sd(data[[y]], na.rm = TRUE)
-    p <-
-      data %>% mutate(R_x = ifelse(is.na(.data[[x]]), NA_level_x, NA),
-                      R_y = ifelse(is.na(.data[[y]]), NA_level_y, NA)) %>%
-      ggplot() +
-      geom_point(aes(x = .data[[x]], y = .data[[y]]),
-                 color = mice:::mdc(1),
-                 na.rm = TRUE) +
-      geom_point(aes(x = .data[[x]], y = R_y),
-                 color = mice:::mdc(2),
-                 na.rm = TRUE) +
-      geom_point(aes(x = R_x, y = .data[[y]]),
-                 color = mice:::mdc(2),
-                 na.rm = TRUE) +
-      scale_x_continuous(expand = expansion(mult = c(0.01, .05))) +
-      scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
-      theme_classic()
-    return(p)
-  } else {
-    # for (ordered) factors and logical variables
-    p <-
-      data %>% mutate(R_x = ifelse(is.na(.data[[x]]), "   ", NA),
-                      R_y = ifelse(is.na(.data[[y]]), "   ", NA)) %>%
-      ggplot() +
-      geom_point(aes(x = .data[[x]], y = .data[[y]]),
-                 color = mice:::mdc(1),
-                 na.rm = TRUE) +
-      # geom_point(aes(x = R_x, y = .data[[y]]),
-      #            color = mice:::mdc(2),
-      #            na.rm = TRUE) +
-      geom_point(aes(x = .data[[x]], y = R_y),
-                 color = mice:::mdc(2),
-                 na.rm = TRUE) +
-      #scale_x_discrete(expand = expansion(mult = c(0.01, .05))) +
-      scale_y_discrete(expand = expansion(mult = c(0.01, .05))) +
-      theme_classic()
-    return(p)
-  }
+  # if (is.numeric(data[[y]])) {
+  #   NA_level_x <-
+  #     min(data[[x]], na.rm = TRUE) - sd(data[[x]], na.rm = TRUE)
+  #   NA_level_y <-
+  #     min(data[[y]], na.rm = TRUE) - sd(data[[y]], na.rm = TRUE)
+  #   p <-
+  #     data %>% mutate(R_x = ifelse(is.na(.data[[x]]), NA_level_x, NA),
+  #                     R_y = ifelse(is.na(.data[[y]]), NA_level_y, NA)) %>%
+  #     ggplot() +
+  #     geom_point(aes(x = .data[[x]], y = .data[[y]]),
+  #                color = mice:::mdc(1),
+  #                na.rm = TRUE) +
+  #     geom_point(aes(x = .data[[x]], y = R_y),
+  #                color = mice:::mdc(2),
+  #                na.rm = TRUE) +
+  #     geom_point(aes(x = R_x, y = .data[[y]]),
+  #                color = mice:::mdc(2),
+  #                na.rm = TRUE) +
+  #     scale_x_continuous(expand = expansion(mult = c(0.01, .05))) +
+  #     scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
+  #     theme_classic()
+  #   return(p)
+  # } else {
+  #   # for (ordered) factors and logical variables
+  p <-
+    data %>% #mutate(R_x = ifelse(is.na(.data[[x]]), "   ", NA),
+    #      R_y = ifelse(is.na(.data[[y]]), "   ", NA)) %>%
+    ggplot() +
+    geom_point(aes(x = .data[[x]], y = .data[[y]]),
+               color = mice:::mdc(1),
+               na.rm = TRUE) +
+    # geom_point(aes(x = R_x, y = .data[[y]]),
+    #            color = mice:::mdc(2),
+    #            na.rm = TRUE) +
+    # geom_point(aes(x = .data[[x]], y = R_y),
+    #            color = mice:::mdc(2),
+    #            na.rm = TRUE) +
+    #scale_x_discrete(expand = expansion(mult = c(0.01, .05))) +
+    #scale_y_discrete(expand = expansion(mult = c(0.01, .05))) +
+    geom_x_list +
+    geom_y_list +
+    scale_x_list +
+    scale_y_list +
+    theme_classic()
+  return(p)
+  
   
 }
 set.seed(123)
