f43404e05625c2cb9948efa378a34ea8d506a5a7
Hanne Oberman <h.i.oberman@uu.nl>
2020-08-13 08:00:33 +0000

    Same with discrete variables

--- R/plot.NA.data.R
+++ R/plot.NA.data.R
@@ -3,17 +3,26 @@
 set.seed(123)
 data <- mice::boys[sample.int(748, 100),]
 
-v = "hc"
+v = "gen"
 
 # data %>% mutate(R = !is.na(.data[[v]]), hc = factor(.data[[v]], exclude = NULL)) %>% 
 #   ggplot() +
 #   geom_point(aes(x = rownames(data), y = .data[[v]], color = R))  +
 #   scale_x_discrete(limits = c(levels(.data[[v]])))
 
+if (is.numeric(data[[v]])){
 NA_level <- min(data[[v]], na.rm = TRUE) - sd(data[[v]], na.rm = TRUE)
 data %>% mutate(R = ifelse(is.na(.data[[v]]), NA_level, NA), id = 1:nrow(data)) %>% 
   ggplot() +
   geom_point(aes(x = id, y = .data[[v]]), color = mice:::mdc(1), na.rm = TRUE) +
   geom_point(aes(x = id, y = R), color = mice:::mdc(2), na.rm = TRUE) + 
   scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
+  theme_classic()
+} else {print("Nope")}
+
+data %>% mutate(R = ifelse(is.na(.data[[v]]), "!!!", NA), id = 1:nrow(data)) %>% 
+  ggplot() +
+  geom_point(aes(x = id, y = .data[[v]]), color = mice:::mdc(1), na.rm = TRUE) +
+  geom_point(aes(x = id, y = R), color = mice:::mdc(2), na.rm = TRUE) + 
+  scale_y_discrete(expand = expansion(mult = c(0.01, .05))) +
   theme_classic()
