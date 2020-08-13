8984e2c5f5f0e8d9ee188d2250a1a47fac7bb895
Hanne Oberman <h.i.oberman@uu.nl>
2020-08-13 09:19:01 +0000

    Add option for completely observed variables to NA plot

--- R/plot.NA.data.R
+++ R/plot.NA.data.R
@@ -1,29 +1,44 @@
 # plot distributions before imputation
-
+plot_NA_margins <- function(data, x) {
+  data <- data %>% mutate(id = 1:nrow(data))
+  # do not do any further processing if there are no NAs
+  if (all(!is.na(data[[x]]))) {
+    return(ggplot(data, aes(x = id, y = .data[[x]])) +
+             geom_point(color = mice:::mdc(1)) +
+             theme_classic())
+  } else {
+    # for continuous variables
+    if (is.numeric(data[[x]])) {
+      NA_level <-
+        min(data[[x]], na.rm = TRUE) - sd(data[[x]], na.rm = TRUE)
+      data %>% mutate(R = ifelse(is.na(.data[[x]]), NA_level, NA)) %>%
+        ggplot() +
+        geom_point(aes(x = id, y = .data[[x]]),
+                   color = mice:::mdc(1),
+                   na.rm = TRUE) +
+        geom_point(aes(x = id, y = R),
+                   color = mice:::mdc(2),
+                   na.rm = TRUE) +
+        scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
+        theme_classic()
+    } else {
+      # for (ordered) factors and logical variables
+      data %>% mutate(R = ifelse(is.na(.data[[x]]), " (NA)", NA)) %>%
+        ggplot() +
+        geom_point(aes(x = id, y = .data[[x]]),
+                   color = mice:::mdc(1),
+                   na.rm = TRUE) +
+        geom_point(aes(x = id, y = R),
+                   color = mice:::mdc(2),
+                   na.rm = TRUE) +
+        scale_y_discrete(expand = expansion(mult = c(0.01, .05))) +
+        theme_classic()
+    }
+  }
+}
 set.seed(123)
-data <- mice::boys[sample.int(748, 100),]
+data <- mice::boys[sample.int(748, 100), ]
 
-v = "phb"
-
-# data %>% mutate(R = !is.na(.data[[v]]), hc = factor(.data[[v]], exclude = NULL)) %>% 
-#   ggplot() +
-#   geom_point(aes(x = rownames(data), y = .data[[v]], color = R))  +
-#   scale_x_discrete(limits = c(levels(.data[[v]])))
-plot_NA_margins <- function(data, x){
-if (is.numeric(data[[v]])){
-NA_level <- min(data[[v]], na.rm = TRUE) - sd(data[[v]], na.rm = TRUE)
-data %>% mutate(R = ifelse(is.na(.data[[v]]), NA_level, NA), id = 1:nrow(data)) %>% 
-  ggplot() +
-  geom_point(aes(x = id, y = .data[[v]]), color = mice:::mdc(1), na.rm = TRUE) +
-  geom_point(aes(x = id, y = R), color = mice:::mdc(2), na.rm = TRUE) + 
-  scale_y_continuous(expand = expansion(mult = c(0.01, .05))) +
-  theme_classic()
-} else {
-data %>% mutate(R = ifelse(is.na(.data[[v]]), " (NA)", NA), id = 1:nrow(data)) %>% 
-  ggplot() +
-  geom_point(aes(x = id, y = .data[[v]]), color = mice:::mdc(1), na.rm = TRUE) +
-  geom_point(aes(x = id, y = R), color = mice:::mdc(2), na.rm = TRUE) + 
-  scale_y_discrete(expand = expansion(mult = c(0.01, .05))) +
-  theme_classic()
-}
-}
+plot_NA_margins(data, x = "hc")
+plot_NA_margins(data, x = "phb")
+plot_NA_margins(data, x = "age")
