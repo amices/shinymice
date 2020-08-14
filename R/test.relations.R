544a2b3becf054f73c561400b70336f9f117f16c
Hanne Oberman <h.i.oberman@uu.nl>
2020-08-14 09:25:01 +0000

    Add glm with standardized regression coefficients to guide imputation model development

--- R/test.relations.R
+++ R/test.relations.R
@@ -22,6 +22,15 @@ test_NA_y <- function(data, x) {
   return(list(top3 = top3, ests = ordered))
 }
 
+# lm.beta <- function (model) 
+# {
+#   b <- summary(model)$coef[-1,1]
+#   sx <- sapply(model$model[-1], sd)
+#   sy <- sapply(model$model[1], sd)
+#   beta <- b * sx /  sy
+#   return(beta)
+# }
+
 #boys %>% test_relations(x = "age")
 
 #purrr::map(names(boys) %>% setNames(., .), ~ test_relations(boys, x = .x))
@@ -37,5 +46,14 @@ test_NA_y <- function(data, x) {
 #
 #
 # boys %>% test_NA_x(x = "tv")
+data = boys
+x = "hgt"
+fam <- ifelse(is.numeric(data[[x]]), "gaussian", "binomial")
+data %>% 
+  mutate(across(where(is.numeric), scale)) %>% 
+  glm(as.formula(paste(x, "~ .")), data = ., family = fam) %>% 
+  broom::tidy() %>% 
+  .[-1,] %>% 
+  dplyr::arrange(desc(abs(estimate)))
 
 # for plotting of logistic regression results, see https://rkabacoff.github.io/datavis/Models.html#logistic-regression
