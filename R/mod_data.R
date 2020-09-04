f67f77fa6348a25fdc8e0ad33108a781ce87d41a
Hanne <h.i.oberman@students.uu.nl>
2020-09-04 13:01:08 +0200

    Hard coded the boys data in the data module to find out why the reactives don't work between modules

--- R/mod_data.R
+++ R/mod_data.R
@@ -1,4 +1,4 @@
-dataUI <- function(id, dat) {
+dataUI <- function(id) {
   tagList(
            tabsetPanel(
              tabPanel(
@@ -24,31 +24,45 @@ dataUI <- function(id, dat) {
 }
 
 dataServer <- function(id, dat) {
-  moduleServer(id, function(input, output, session, data = dat) {
+  moduleServer(id, function(input, output, session) {
   
   # stopifnot(is.reactive(data))
-    
-    vars = names(data)
+  #data <- reactive(mice::boys)
+  vars <- reactive(names(dat))
+  data <- reactive(mice::boys)
     
   output$table <-
   renderDT({
-    DT_NA_highlight(data, vars)
+    DT_NA_highlight(data(), vars())
   }, server = F)
 
 #output$n <- renderText(paste0("Descriptive statistics (n = ", nrow(data()), ")"))
 output$descr <- renderDT(
-  descr(data) %>% 
+  descr(data()) %>% 
     dplyr::mutate_if(is.numeric, round, 2) %>%
     DT::datatable(options = list(pageLength = 5)) %>%
     DT::formatStyle(
       "n",
       target = "cell",
-      color = DT::styleInterval(c(nrow(data)-1), c("#B61A51", "black")),
-      fontWeight = DT::styleInterval(c(nrow(data)-1), c("bold", "")),
+      color = DT::styleInterval(c(nrow(data())-1), c("#B61A51", "black")),
+      fontWeight = DT::styleInterval(c(nrow(data())-1), c("bold", "")),
     )
   
 )
 
-output$categ <- renderDT(least_perc(data))
+output$categ <- renderDT(least_perc(data()))
 
-})}
+  })}
+
+# # test the module
+# source("R/fnc_tabulate.NA.data.R")
+# dat = mice::boys
+# testApp <- function() {
+#   ui <- fluidPage(
+#     dataUI("hist1")
+#   )
+#   server <- function(input, output, session) {
+#     dataServer("hist1", data = dat)
+#   }
+#   shinyApp(ui, server)
+# }
