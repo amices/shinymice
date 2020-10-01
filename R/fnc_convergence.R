f57ea4d11324f119c8328a913adbd538fa22c79d
Oberman <h.i.oberman@uu.nl>
2020-10-01 15:01:01 +0200

    rank-normalize rhat

--- R/fnc_convergence.R
+++ R/fnc_convergence.R
@@ -31,6 +31,7 @@ bulk <- out %>%
 # split chains
 mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% #rep(1:5, each = 10) + c(rep(0, 5), rep(5, 5)))#map(1:5, function(i){rep(c(i, i+5), each = 5)}) %>% unlist())#rep(c(1:ceiling(max(out$m) / 2), 1:floor(max(out$m)/2)), nit)) %>% 
 # rank-normalize
+  mutate("mu.Y" = qnorm((rank(.data[["mu.Y"]])-3/8)/(nrow(out)-1/4))) %>% 
 # get rhat
   group_by(m) %>% 
   summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
