670cec8003ac6ee4d1eba1858ad41cee50cc9164
Oberman <h.i.oberman@uu.nl>
2020-10-01 15:27:14 +0200

    fold chains rhat

--- R/fnc_convergence.R
+++ R/fnc_convergence.R
@@ -29,10 +29,26 @@ OG <- out %>%
 
 bulk <- out %>% 
 # split chains
-mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% #rep(1:5, each = 10) + c(rep(0, 5), rep(5, 5)))#map(1:5, function(i){rep(c(i, i+5), each = 5)}) %>% unlist())#rep(c(1:ceiling(max(out$m) / 2), 1:floor(max(out$m)/2)), nit)) %>% 
+  mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% 
 # rank-normalize
   mutate("mu.Y" = qnorm((rank(.data[["mu.Y"]])-3/8)/(nrow(out)-1/4))) %>% 
 # get rhat
+  group_by(m) %>% 
+  summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
+  summarise(B = var(mean) * nit, W = mean(var)) %>% 
+  mutate(varplus = (((nit-1)/nit)*W) + B/nit,
+         rhat = sqrt(varplus/W))  %>% 
+  select(rhat) %>% 
+  as.double()
+
+tails <- out %>% 
+  # fold chains
+  mutate("mu.Y" = abs(.data[["mu.Y"]] - median(.data[["mu.Y"]]))) %>% 
+  # split chains
+  mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% 
+  # rank-normalize
+  mutate("mu.Y" = qnorm((rank(.data[["mu.Y"]])-3/8)/(nrow(out)-1/4))) %>% 
+  # get rhat
   group_by(m) %>% 
   summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
   summarise(B = var(mean) * nit, W = mean(var)) %>% 
