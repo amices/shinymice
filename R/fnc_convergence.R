efdf64a0d0e8ec420a05301d3561a8f969d6089e
Oberman <h.i.oberman@uu.nl>
2020-10-01 16:31:11 +0200

    rhat for each var

--- R/fnc_convergence.R
+++ R/fnc_convergence.R
@@ -4,55 +4,55 @@
 
 ext = "mu."
 nit <- mids$iteration
-out <- mids$chainMean %>% 
-  as.data.frame() %>% 
-  t() %>% 
+out <- mids$chainMean %>%
+  as.data.frame() %>%
+  t() %>%
   as.data.frame() %>%
   setNames(paste0(ext, names(.))) %>%
-  cbind(
-    it = 1:nit,
-    m = rep(1:mids$m, each = nit),
-    .
-  )
+  cbind(it = 1:nit,
+        m = rep(1:mids$m, each = nit),
+        .)
 rownames(out) <- NULL
 out
 
-OG <- out %>% 
-  # get rhat
-  group_by(m) %>% 
-  summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
-  summarise(B = var(mean) * nit, W = mean(var)) %>% 
-  mutate(varplus = (((nit-1)/nit)*W) + B/nit,
-         rhat = sqrt(varplus/W)) %>% 
-  select(rhat) %>% 
-  as.double()
+rhat <- function(d, v) {
+  nit <- max(d$it)
+  d %>%
+    group_by(m) %>%
+    summarise(mean = mean(.data[[v]]), var = var(.data[[v]])) %>%
+    summarise(B = var(mean) * nit, W = mean(var)) %>%
+    mutate(varplus = (((nit - 1) / nit) * W) + B / nit,
+           rhat = sqrt(varplus / W)) %>%
+    select(rhat) 
+}
 
-bulk <- out %>% 
-# split chains
-  mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% 
-# rank-normalize
-  mutate("mu.Y" = qnorm((rank(.data[["mu.Y"]])-3/8)/(nrow(out)-1/4))) %>% 
-# get rhat
-  group_by(m) %>% 
-  summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
-  summarise(B = var(mean) * nit, W = mean(var)) %>% 
-  mutate(varplus = (((nit-1)/nit)*W) + B/nit,
-         rhat = sqrt(varplus/W))  %>% 
-  select(rhat) %>% 
-  as.double()
-
-tails <- out %>% 
-  # fold chains
-  mutate("mu.Y" = abs(.data[["mu.Y"]] - median(.data[["mu.Y"]]))) %>% 
-  # split chains
-  mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% 
-  # rank-normalize
-  mutate("mu.Y" = qnorm((rank(.data[["mu.Y"]])-3/8)/(nrow(out)-1/4))) %>% 
+purrr::map_dfc(names(out)[-c(1:2)], function(vrb){
+OG <- out %>%
   # get rhat
-  group_by(m) %>% 
-  summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
-  summarise(B = var(mean) * nit, W = mean(var)) %>% 
-  mutate(varplus = (((nit-1)/nit)*W) + B/nit,
-         rhat = sqrt(varplus/W))  %>% 
-  select(rhat) %>% 
-  as.double()
+  rhat(d = ., v = vrb)
+
+bulk <- out %>%
+  mutate(# split chains
+    m = m +
+      c(rep(0, ceiling(nit / 2)), rep(ceiling(nit / 2), floor(nit / 2))),
+    # rank-normalize
+    vrb = qnorm((rank(.data[[vrb]]) - 3 / 8) / (nrow(out) - 1 / 4))) %>%
+  # get rhat
+  rhat(d = ., v = vrb)
+
+tails <- out %>%
+  mutate(
+    # fold chains
+    vrb = abs(.data[[vrb]] - median(.data[[vrb]])),
+    # split chains
+    m = m +
+      c(rep(0, ceiling(nit / 2)), rep(ceiling(nit / 2), floor(nit / 2))),
+    # rank-normalize
+    vrb = qnorm((rank(.data[[vrb]]) - 3 / 8) / (nrow(out) - 1 / 4))
+  ) %>%
+  # get rhat
+  rhat(d = ., v = vrb)
+
+data.frame(max(OG, bulk, tails)) %>% 
+  setNames(paste0("rhat.", vrb))
+}) %>% cbind(it = nit, .)
