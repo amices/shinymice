31ca4f99aface15bf403c8166ca8c2c6c22f2f62
Oberman <h.i.oberman@uu.nl>
2020-10-01 14:40:33 +0200

    split chains rhat

--- R/fnc_convergence.R
+++ R/fnc_convergence.R
@@ -17,9 +17,25 @@ out <- mids$chainMean %>%
 rownames(out) <- NULL
 out
 
-out %>% 
+OG <- out %>% 
+  # get rhat
   group_by(m) %>% 
   summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
   summarise(B = var(mean) * nit, W = mean(var)) %>% 
   mutate(varplus = (((nit-1)/nit)*W) + B/nit,
-         rhat = sqrt(varplus/W))
+         rhat = sqrt(varplus/W)) %>% 
+  select(rhat) %>% 
+  as.double()
+
+bulk <- out %>% 
+# split chains
+mutate(m = m + c(rep(0, ceiling(nit/2)), rep(ceiling(nit/2), floor(nit/2)))) %>% #rep(1:5, each = 10) + c(rep(0, 5), rep(5, 5)))#map(1:5, function(i){rep(c(i, i+5), each = 5)}) %>% unlist())#rep(c(1:ceiling(max(out$m) / 2), 1:floor(max(out$m)/2)), nit)) %>% 
+# rank-normalize
+# get rhat
+  group_by(m) %>% 
+  summarise(mean = mean(.data[["mu.Y"]]), var = var(.data[["mu.Y"]])) %>% 
+  summarise(B = var(mean) * nit, W = mean(var)) %>% 
+  mutate(varplus = (((nit-1)/nit)*W) + B/nit,
+         rhat = sqrt(varplus/W))  %>% 
+  select(rhat) %>% 
+  as.double()
