444e0bf40dc26da90bfccb095bd6c65c6ceee26d
Oberman <h.i.oberman@uu.nl>
2020-10-27 11:04:56 +0100

    Fix spacing of shiny inputs with cleaner solution, add github link

--- shinymice/R/mod_home.R
+++ shinymice/R/mod_home.R
@@ -14,12 +14,9 @@ mod_home_ui <- function(id){
       div(img(src = "www/favicon.png", width = 300), align = "center"),#h2("[shinymice logo here]", align = "center"),
       br(),
       h2("[navigation buttons here]", align = "center"),
-      h5("[link to github repo for issues]", align = "center"),
+      h5(" ", align = "center"),
+      div("For the most recent version of this app or to report an issue, see ", tags$a(href = "https://github.com/amices/shinyMice", "github.com/amices/shinyMice"), ".", align = "center"),
       br()
-      # br(),
-      # h2("[upload data here]", align = "center"),
-      # br(),
-      # verbatimTextOutput(ns("name"))
     )
     )
 }
@@ -30,7 +27,6 @@ mod_home_ui <- function(id){
 mod_home_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    #output$name <- renderText("[print name of dataset here?]")
   })
 }
     
