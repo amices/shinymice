5e6a81a94829ceea3ceb4675beb2bc5fade88ca2
Oberman <h.i.oberman@uu.nl>
2020-10-28 17:59:22 +0100

    Fix rounding issue with descriptives table

--- shinymice/R/mod_imputeddata_utils_descr.R
+++ shinymice/R/mod_imputeddata_utils_descr.R
@@ -5,6 +5,7 @@ imp_descr <- function(imp){
     purrr::map_df(., ~{psych::describe(.)[,c("n", "mean", "sd", "min", "max", "median")] %>% 
     cbind(., variable = rownames(.))}) %>% 
     aggregate(.~variable, data = ., FUN = "mean") %>% 
-    cbind(., n_imputed = imp$nmis)
+    cbind(., n_imputed = as.integer(imp$nmis))
+  tab[,2] <- as.integer(tab[,2])
   return(tab)
 }
--- shinymice/R/mod_missingness_utils_descr.R
+++ shinymice/R/mod_missingness_utils_descr.R
@@ -1,7 +1,9 @@
 # descriptive statistics for incomplete data
 library(dplyr)
+options(htmlwidgets.TOJSON_ARGS = list(na = 'string'))
 mis_descr <- function(d){
   tab <- psych::describe(d)[,c("n", "mean", "sd", "min", "max", "median")] %>% 
-    cbind(variable = rownames(.), ., n_missing = nrow(d) - .$n)
+    cbind(variable = rownames(.), ., n_missing = as.integer(nrow(d) - .$n))
+  tab[,2] <- as.integer(tab[,2])
   return(tab)
 }
