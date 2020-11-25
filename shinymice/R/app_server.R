87d2b13e5d9f7cc8b97daa66baa9e0c6c3522551
Oberman <h.i.oberman@uu.nl>
2020-11-25 11:22:07 +0100

    Made the mids object reactive

--- shinymice/R/app_server.R
+++ shinymice/R/app_server.R
@@ -8,8 +8,8 @@ app_server <- function(input, output, session) {
   # List the first level callModules here
   mod_home_server("home_ui_1")
   data <- mod_missingness_server("missingness_ui_1")
-  mod_imputationmodel_server("imputationmodel_ui_1", data)
-  mod_imputeddata_server("imputeddata_ui_1")
+  imp <- mod_imputationmodel_server("imputationmodel_ui_1", data)
+  mod_imputeddata_server("imputeddata_ui_1", imp)
   mod_scientificmodel_server("scientificmodel_ui_1")
   mod_save_server("save_ui_1")
 }
